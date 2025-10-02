a1883d8774e00b7d0ad816a9dea02319b00fdbfe
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2025-10-02 16:58:17 -0400

    Update julia.js

---
+++ julia.js
@@ -0,0 +1,12 @@
+// soma.js
+
+function soma(a, b) {
+    if (a < 0 || b < 0) {
+        return "Erro: não permite números negativos";
+    }
+    return a + b;
+}
+
+function subtrai(a, b) {
+    return a - b;
+}
